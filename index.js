ce38a24ac184d2238865ae8d3161320fc1d0cc15
JohnRamberger <jramberger3@gatech.edu>
2022-07-09 16:00:19 -0400

    setup express and react

---
+++ index.js
@@ -0,0 +1,34 @@
+const path = require("node:path");
+
+//setup express
+const express = require("express");
+const app = express();
+
+//load .env
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4211;
+
+//setup logger
+// const { log } = require("./logger");
+
+//setup routes
+// const api = require("./routes/api");
+
+// app.use("/api", api);
+
+// io.on("connection", function (client) {
+//     //log("client connected.");
+// });
+
+//setup static react build
+// app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("/*", (req, res, next) => {
+    // res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.send("hello world!");
+    res.status(200);
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+});
