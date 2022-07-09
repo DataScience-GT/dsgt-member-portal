999c8ba4daba9df4722ce82f4334a64fe54e6089
JohnRamberger <jramberger3@gatech.edu>
2022-07-09 16:31:45 -0400

    setup

---
+++ Auth.js
@@ -0,0 +1,5 @@
+const asd = () => {
+
+}
+
+export default asd;
--- index.js
+++ index.js
@@ -12,23 +12,21 @@ const PORT = process.env.PORT || 4211;
 //setup logger
 // const { log } = require("./logger");
 
-//setup routes
-// const api = require("./routes/api");
+// setup routes
+const api = require("./routes/api");
 
-// app.use("/api", api);
+app.use("/api", api);
 
 // io.on("connection", function (client) {
 //     //log("client connected.");
 // });
 
-//setup static react build
-// app.use(express.static(path.join(__dirname, "client", "build")));
+// setup static react build
+app.use(express.static(path.join(__dirname, "client", "build")));
 app.get("/*", (req, res, next) => {
-    // res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-    res.send("hello world!");
-    res.status(200);
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
+    console.log(`App listening on port ${PORT}`);
 });
