221e19e472e602eb0c9ae4a18c7ca7f3a4306727
JohnRamberger <jramberger3@gatech.edu>
2022-07-17 22:24:29 -0400

    added inputs

--- client/src/Pages/Signup/Signup.tsx
+++ client/src/Pages/Signup/Signup.tsx
@@ -4,13 +4,45 @@ import styles from "./Signup.module.scss";
 
 interface SignupProps {}
 
-const Signup: FC<SignupProps> = () => (
-  <div className={styles.Signup} data-testid="Signup">
-    <InputField type="text" placeholder="First Name" />
-    <InputField type="text" placeholder="Last Name" />
-    <InputField type="email" placeholder="Email" />
-    <InputField type="password" placeholder="Password" />
-  </div>
-);
+const Signup: FC<SignupProps> = () => {
+  const handleChange_fname = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+  const handleChange_lname = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+  const handleChange_email = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+  const handleChange_password = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+  return (
+    //introduce states for each value and prevent/allow certain chars
+    <div className={styles.Signup} data-testid="Signup">
+      <InputField
+        type="text"
+        placeholder="First Name"
+        onChange={handleChange_fname}
+      />
+      <InputField
+        type="text"
+        placeholder="Last Name"
+        onChange={handleChange_lname}
+      />
+      <InputField
+        type="email"
+        placeholder="Email"
+        onChange={handleChange_email}
+      />
+      <InputField
+        type="password"
+        placeholder="Password"
+        onChange={handleChange_password}
+      />
+      <InputField type="submit" />
+    </div>
+  );
+};
 
 export default Signup;
--- client/src/components/InputField/InputField.tsx
+++ client/src/components/InputField/InputField.tsx
@@ -4,21 +4,53 @@ import styles from "./InputField.module.scss";
 interface InputFieldProps {
   type?: React.HTMLInputTypeAttribute;
   placeholder?: string;
+  width?: string;
+  onClick?: React.MouseEventHandler<HTMLInputElement>;
+  onChange?: React.KeyboardEventHandler<HTMLInputElement>;
 }
 
 const InputField: FC<InputFieldProps> = ({
   type,
   placeholder,
-}: InputFieldProps) => (
-  <div className={styles.InputField} data-testid="InputField">
-    <input
-      id={"input-" + placeholder?.replaceAll(" ", "-")}
-      type={type}
-      placeholder=" "
-      required
-    />
-    <label htmlFor={"input-" + placeholder}>{placeholder}</label>
-  </div>
-);
+  width,
+  onClick,
+  onChange,
+}: InputFieldProps) => {
+  if (type === "submit") {
+    return (
+      <div
+        className={styles.InputFieldSubmit}
+        data-testid="InputField"
+        style={{ width: width || "300px" }}
+      >
+        <input
+          id={"input-" + placeholder?.replaceAll(" ", "-")}
+          type="submit"
+          onClick={onClick}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className={styles.InputField}
+        data-testid="InputField"
+        style={{ width: width || "300px" }}
+      >
+        <input
+          id={"input-" + placeholder?.replaceAll(" ", "-")}
+          type={type}
+          placeholder=" "
+          required
+          onChange={onChange}
+        />
+        <label htmlFor={"input-" + placeholder}>
+          {placeholder}
+          <span>✓</span>
+        </label>
+      </div>
+    );
+  }
+};
 
 export default InputField;
