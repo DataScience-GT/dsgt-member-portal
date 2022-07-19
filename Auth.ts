0a97bd10bcc74db6c0ec44c9fe969321b12beb4b
JohnRamberger <jramberger3@gatech.edu>
2022-07-18 22:44:00 -0400

    Update Auth.ts
    moved from basic auth to bearer tokens

--- Auth.ts
+++ Auth.ts
@@ -8,28 +8,52 @@ const apiAuthenticate = (
   next: NextFunction
 ): void => {
   if (req && req.headers && req.headers.authorization) {
-    let auth = Buffer.from(
-      req.headers.authorization.split(" ")[1],
-      "base64"
-    ).toString();
+    if (req.headers.authorization.includes("Bearer ")) {
+      let auth = req.headers.authorization.split(" ")[1];
 
-    //auth = username:password
-    let key = auth.split(":")[0];
-    let pass = auth.split(":")[1];
-
-    if (
-      key === process.env.API_AUTH_KEY &&
-      pass === process.env.API_AUTH_PASS
-    ) {
-      next();
+      if (auth === process.env.API_KEY) {
+        next();
+      } else {
+        let err = new Error("invalid bearer token");
+        next(err);
+      }
     } else {
-      let err = new Error("invalid basic authorization");
+      let err = new Error("missing authorization header");
       next(err);
     }
-  } else {
-    let err = new Error("missing authorization header");
-    next(err);
   }
 };
+
+//--------------------- used for basic auth ---------------------
+
+// const apiAuthenticate = (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ): void => {
+//   if (req && req.headers && req.headers.authorization) {
+//     let auth = Buffer.from(
+//       req.headers.authorization.split(" ")[1],
+//       "base64"
+//     ).toString();
+
+//     //auth = username:password
+//     let key = auth.split(":")[0];
+//     let pass = auth.split(":")[1];
+
+//     if (
+//       key === process.env.API_AUTH_KEY &&
+//       pass === process.env.API_AUTH_PASS
+//     ) {
+//       next();
+//     } else {
+//       let err = new Error("invalid basic authorization");
+//       next(err);
+//     }
+//   } else {
+//     let err = new Error("missing authorization header");
+//     next(err);
+//   }
+// };
 
 export { apiAuthenticate };
