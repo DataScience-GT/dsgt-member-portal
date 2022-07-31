094f028f7ce9e32fae5e2e0ffa57b63313da3d50
JohnRamberger <jramberger3@gatech.edu>
2022-07-31 12:53:52 -0400

    Update 20220731164838_payment_details.ts

--- data/migrations/20220731164838_payment_details.ts
+++ data/migrations/20220731164838_payment_details.ts
@@ -1,10 +1,34 @@
 import { Knex } from "knex";
 
+// export interface BillingDetails {
+//     address: {
+//       city: string;
+//       country: string;
+//       line1: string;
+//       line2?: string;
+//       postal_code: number | string;
+//       state: string;
+//     };
+//     email: string;
+//     name: string;
+//     phone?: string;
+//   }
 
 export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("billing_details", function (table) {
+    table.increments("details_id").primary();
+    table.string("city");
+    table.string("country");
+    table.string("line1");
+    table.string("line2");
+    table.string("postal_code");
+    table.string("state");
+    table.string("email");
+    table.string("name");
+    table.string("phone");
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists("billing_details");
 }
-
