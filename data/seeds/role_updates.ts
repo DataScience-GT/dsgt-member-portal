55f01022c7a4ed85a980e458d2ed3a0317bb970d
JohnRamberger <jramberger3@gatech.edu>
2022-08-15 17:05:13 -0400

    Update role_updates.ts

--- data/seeds/role_updates.ts
+++ data/seeds/role_updates.ts
@@ -2,5 +2,5 @@ import { Knex } from "knex";
 
 export async function seed(knex: Knex): Promise<void> {
     // Inserts seed entries
-    await knex("user").update({role: "administrator"}).where("email", "ssaraf33@gatech.edu")
+    await knex("user").update({role: "administrator"}).where("email", "ssaraf33@gatech.edu");
 };
