3c6ff2f5a1255f9b83524c1cf59381268619f84c
JohnRamberger <jramberger3@gatech.edu>
2022-08-17 12:17:54 -0400

    Update PortalForms.tsx

--- client/src/Pages/PortalForms/PortalForms.tsx
+++ client/src/Pages/PortalForms/PortalForms.tsx
@@ -7,8 +7,14 @@ interface PortalFormsProps {}
 
 const PortalForms: FC<PortalFormsProps> = () => (
   <div className={styles.PortalForms} data-testid="PortalForms">
+    <h1 className={styles.Major}>Forms</h1>
+    <h1 className={styles.Minor}>Available Forms</h1>
     <FlexColumn gap="10px">
-      <FormItem formName="Leadership Application" formTime="5min" formLink="#" />
+      <FormItem
+        formName="Leadership Application"
+        formTime="5min"
+        formLink="#"
+      />
       <FormItem />
       <FormItem />
     </FlexColumn>
