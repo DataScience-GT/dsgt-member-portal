8fc921b864d9b4a9a213a962c3bcd4e669e82c61
JohnRamberger <jramberger3@gatech.edu>
2022-08-17 12:17:02 -0400

    added FormItem
    item for displaying forms to users.

--- client/src/Pages/PortalForms/PortalForms.tsx
+++ client/src/Pages/PortalForms/PortalForms.tsx
@@ -1,11 +1,17 @@
-import React, { FC } from 'react';
-import styles from './PortalForms.module.scss';
+import React, { FC } from "react";
+import FormItem from "../../components/FormItem/FormItem";
+import FlexColumn from "../../layout/FlexColumn/FlexColumn";
+import styles from "./PortalForms.module.scss";
 
 interface PortalFormsProps {}
 
 const PortalForms: FC<PortalFormsProps> = () => (
   <div className={styles.PortalForms} data-testid="PortalForms">
-    PortalForms Component
+    <FlexColumn gap="10px">
+      <FormItem formName="Leadership Application" formTime="5min" formLink="#" />
+      <FormItem />
+      <FormItem />
+    </FlexColumn>
   </div>
 );
 
---
+++ client/src/components/FormItem/FormItem.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from "react";
+import FlexRow from "../../layout/FlexRow/FlexRow";
+import styles from "./FormItem.module.scss";
+
+interface FormItemProps {
+  formName?: string;
+  formTime?: string;
+  formLink?: string;
+}
+
+const FormItem: FC<FormItemProps> = ({
+  formName,
+  formTime,
+  formLink,
+}: FormItemProps) => (
+  <div className={styles.FormItem} data-testid="FormItem">
+    <a href={formLink} target="_blank" rel="noreferrer">
+      <FlexRow align="center">
+        <div className={styles.LabelBox}>
+          <span className={styles.FormLabel}>Form</span>
+        </div>
+        <div className={styles.FormContent}>
+          <span className={styles.FormName}>{formName || "form name"}</span>
+
+          {formTime ? (
+            <span
+              className={styles.FormTime}
+            >{`Estimated Time: ${formTime}`}</span>
+          ) : (
+            ""
+          )}
+        </div>
+
+        <span className={styles.FormArrow}>→</span>
+      </FlexRow>
+    </a>
+  </div>
+);
+
+export default FormItem;
