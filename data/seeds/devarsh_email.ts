268c080c2819fa7173ef332eb0d6f8cf0badf352
JohnRamberger <jramberger3@gatech.edu>
2022-08-21 18:30:07 -0400

    Update devarsh_email.ts

--- data/seeds/devarsh_email.ts
+++ data/seeds/devarsh_email.ts
@@ -3,6 +3,6 @@ import { Knex } from "knex";
 export async function seed(knex: Knex): Promise<void> {
   //change devarshes email
   await knex("user")
-    .update({ email: "Devarshspandya@gmail.com" })
-    .where("email", "=", "snehalpandya@hotmail.com");
+    .update({ email: "devarshspandya@gmail.com" })
+    .where("email", "=", "Devarshspandya@gmail.com");
 }
