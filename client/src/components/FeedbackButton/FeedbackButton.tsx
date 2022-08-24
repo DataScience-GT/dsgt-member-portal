edc5b07fb49dd32ab0c9b27fa5e602217ec80e2e
JohnRamberger <jramberger3@gatech.edu>
2022-08-24 11:52:56 -0400

    updated FeedbackMenu
    added more fields (satisfied, change, feature, bug) along with textarea inputs

--- client/src/components/FeedbackButton/FeedbackButton.tsx
+++ client/src/components/FeedbackButton/FeedbackButton.tsx
@@ -5,21 +5,33 @@ import styles from "./FeedbackButton.module.scss";
 import feedback_icon from "../../assets/icons/comments-question.svg";
 import close_icon from "../../assets/icons/cross-small-skinny.svg";
 import InlineRadioInput from "../InlineRadioInput/InlineRadioInput";
+import FlexColumn from "../../layout/FlexColumn/FlexColumn";
+import { handleChange_textarea_string } from "../../Scripts/InputHandler";
 
 interface FeedbackButtonProps {}
 
 const FeedbackButton: FC<FeedbackButtonProps> = () => {
-  // const [open, setOpen] = useState(false);
+  const [screen, setScreen] = useState(1);
 
-  // const handleOpen = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   console.log(e.currentTarget.checked);
-  //   setOpen(e.currentTarget.checked);
-  // };
-
+  const [satisfied, setSatisfied] = useState("");
   const [action, setAction] = useState("");
+  const [urgency, setUrgency] = useState("");
+  const [bugText, setBugText] = useState("");
+  const [changeText, setChangeText] = useState("");
+  const [featureText, setFeatureText] = useState("");
 
-  const handleCloseMenu = (e: React.MouseEvent<HTMLImageElement>) => {
+  const handleCloseMenu = () => {
     document.getElementById("feedback-menu-open")?.click();
+  };
+
+  const handleSubmit = () => {
+    console.log(satisfied, action, urgency, bugText, changeText, featureText);
+
+    setScreen(2);
+    setTimeout(() => {
+      handleCloseMenu();
+      setScreen(1);
+    }, 2000);
   };
 
   return (
@@ -56,28 +68,163 @@ const FeedbackButton: FC<FeedbackButtonProps> = () => {
             onClick={handleCloseMenu}
           />
         </div>
-        <h1 className={styles.Major}>Feedback</h1>
-        <h1 className={styles.Minor}>Would you like to:</h1>
-        <FlexRow padding="0.5em 0" gap="0.5em">
-          <InlineRadioInput
-            name="test"
-            label="Suggest a new feature"
-            color="green"
-            value="feature"
-            currentValue={action}
-            setValue={setAction}
-          />
-          <InlineRadioInput
-            name="test"
-            label="Report a bug"
-            color="red"
-            value="bug"
-            currentValue={action}
-            setValue={setAction}
-          />
-        </FlexRow>
-        {action === "feature" ? <p>a</p> : ""}
-        {action === "bug" ? <p>b</p> : ""}
+        {screen === 1 ? (
+          <>
+            <h1 className={styles.Major}>Feedback</h1>
+            <h1 className={styles.Minor}>
+              How satisfied are you with this membership portal?
+            </h1>
+            <FlexRow padding="0.5em 0" gap="0.5em">
+              <InlineRadioInput
+                name="satisfied"
+                label="Very Satisfied"
+                color="green"
+                currentValue={satisfied}
+                setValue={setSatisfied}
+              />
+              <InlineRadioInput
+                name="satisfied"
+                label="Satisfied"
+                color="#99cc00"
+                currentValue={satisfied}
+                setValue={setSatisfied}
+              />
+              <InlineRadioInput
+                name="satisfied"
+                label="Neither Satisfied nor Unsatisfied"
+                color="#aaa"
+                currentValue={satisfied}
+                setValue={setSatisfied}
+              />
+              <InlineRadioInput
+                name="satisfied"
+                label="Unsatisfied"
+                color="#FD9800"
+                currentValue={satisfied}
+                setValue={setSatisfied}
+              />
+              <InlineRadioInput
+                name="satisfied"
+                label="Very Unsatisfied"
+                color="red"
+                currentValue={satisfied}
+                setValue={setSatisfied}
+              />
+            </FlexRow>
+            <h1 className={styles.Minor}>Would you like to:</h1>
+            <FlexRow padding="0.5em 0" gap="0.5em">
+              <InlineRadioInput
+                name="action"
+                label="Suggest a new feature"
+                color="green"
+                value="feature"
+                currentValue={action}
+                setValue={setAction}
+              />
+              <InlineRadioInput
+                name="action"
+                label="Suggest a change"
+                color="#FD9800"
+                value="change"
+                currentValue={action}
+                setValue={setAction}
+              />
+              <InlineRadioInput
+                name="action"
+                label="Report a bug"
+                color="red"
+                value="bug"
+                currentValue={action}
+                setValue={setAction}
+              />
+            </FlexRow>
+            {action === "feature" ? (
+              <>
+                <h1 className={styles.Minor}>
+                  Please describe the feature you would like in detail:
+                </h1>
+                <textarea
+                  className={styles.TextInput}
+                  onChange={(e) => {
+                    handleChange_textarea_string(e, setFeatureText);
+                  }}
+                ></textarea>
+              </>
+            ) : (
+              ""
+            )}
+            {action === "change" ? (
+              <>
+                <h1 className={styles.Minor}>
+                  Please describe the change you would like:
+                </h1>
+                <textarea
+                  className={styles.TextInput}
+                  onChange={(e) => {
+                    handleChange_textarea_string(e, setChangeText);
+                  }}
+                ></textarea>
+              </>
+            ) : (
+              ""
+            )}
+            {action === "bug" ? (
+              <>
+                <h1 className={styles.Minor}>Urgency:</h1>
+                <FlexRow padding="0.5em 0" gap="0.5em">
+                  <InlineRadioInput
+                    name="urgency"
+                    label="High"
+                    color="red"
+                    currentValue={urgency}
+                    setValue={setUrgency}
+                  />
+                  <InlineRadioInput
+                    name="urgency"
+                    label="Medium"
+                    color="#FD9800"
+                    currentValue={urgency}
+                    setValue={setUrgency}
+                  />
+                  <InlineRadioInput
+                    name="urgency"
+                    label="Low"
+                    color="#B5D6A7"
+                    currentValue={urgency}
+                    setValue={setUrgency}
+                  />
+                </FlexRow>
+                <h1 className={styles.Minor}>
+                  Please describe where you found the bug and what it does:
+                </h1>
+                <textarea
+                  className={styles.TextInput}
+                  onChange={(e) => {
+                    handleChange_textarea_string(e, setBugText);
+                  }}
+                ></textarea>
+              </>
+            ) : (
+              ""
+            )}
+            <input
+              className={styles.SubmitButton}
+              type="submit"
+              onClick={handleSubmit}
+            />
+          </>
+        ) : (
+          <FlexColumn
+            width="100%"
+            height="16em"
+            spacing="center"
+            align="center"
+          >
+            <h1 className={styles.Major} style={{ textAlign: "center" }}>
+              Thank You!
+            </h1>
+          </FlexColumn>
+        )}
       </div>
     </div>
   );
