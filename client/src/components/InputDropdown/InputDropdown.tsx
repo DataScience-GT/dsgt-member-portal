67c0222a261fe231ed2e4d53d0a44d00c385b22c
JohnRamberger <jramberger3@gatech.edu>
2022-08-24 22:37:36 -0400

    Update InputDropdown.tsx
    added values input (to have different values than displayed)

--- client/src/components/InputDropdown/InputDropdown.tsx
+++ client/src/components/InputDropdown/InputDropdown.tsx
@@ -3,6 +3,7 @@ import styles from "./InputDropdown.module.scss";
 
 interface InputDropdownProps {
   options?: any[];
+  values?: any[];
   initialValue?: any;
   hideInitial?: boolean;
   setState?: React.Dispatch<React.SetStateAction<any>>;
@@ -11,48 +12,61 @@ interface InputDropdownProps {
 
 const InputDropdown: FC<InputDropdownProps> = ({
   options,
+  values,
   initialValue,
   hideInitial,
   setState,
   onChange,
-}: InputDropdownProps) => (
-  <div className={styles.InputDropdown} data-testid="InputDropdown">
-    <select
-      className={styles.DropDown}
-      onChange={(e) => {
-        if (setState) setState(e.currentTarget.value);
-        if (onChange) onChange(e);
-      }}
-      value={initialValue}
-    >
-      {options
-        ? options.map((o, i) => {
-            if (o === initialValue) {
-              if (hideInitial) {
-                return "";
+}: InputDropdownProps) => {
+  // console.log(initialValue);
+  return (
+    <div className={styles.InputDropdown} data-testid="InputDropdown">
+      <select
+        className={styles.DropDown}
+        onChange={(e) => {
+          if (setState) setState(e.currentTarget.value);
+          if (onChange) onChange(e);
+        }}
+        value={initialValue}
+      >
+        {options
+          ? options.map((o, i) => {
+              if (o === initialValue) {
+                if (hideInitial) {
+                  return "";
+                } else {
+                  return (
+                    <option
+                      key={i}
+                      value={
+                        values && values.length === options.length
+                          ? values[i]
+                          : o
+                      }
+                      // selected
+                      style={{ fontWeight: 700 }}
+                    >
+                      {o}
+                    </option>
+                  );
+                }
               } else {
                 return (
                   <option
                     key={i}
-                    value={o}
-                    // selected
-                    style={{ fontWeight: 700 }}
+                    value={
+                      values && values.length === options.length ? values[i] : o
+                    }
                   >
                     {o}
                   </option>
                 );
               }
-            } else {
-              return (
-                <option key={i} value={o}>
-                  {o}
-                </option>
-              );
-            }
-          })
-        : ""}
-    </select>
-  </div>
-);
+            })
+          : ""}
+      </select>
+    </div>
+  );
+};
 
 export default InputDropdown;
