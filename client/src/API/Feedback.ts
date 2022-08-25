4054b41d322c2c8880677303c3437544e33cb0b3
JohnRamberger <jramberger3@gatech.edu>
2022-08-24 22:20:51 -0400

    updated PortalFeedback
    added fetching from api

--- client/src/API/Feedback.ts
+++ client/src/API/Feedback.ts
@@ -1,3 +1,50 @@
+export type result_getFeedback = {
+  feedback_id: number;
+  user_id?: number;
+  satisfaction?: string;
+  action?: FeedbackType;
+  urgency?: number;
+  content?: string;
+  created_at: string;
+  resolved: boolean;
+  resolved_by?: number;
+};
+
+export enum FeedbackType {
+  Bugs = "bug",
+  Changes = "change",
+  Features = "feature",
+}
+
+export const getFeedback = async (
+  type: FeedbackType,
+  count?: number,
+  callback?: (result: result_getFeedback[]) => void
+) => {
+  let url = "/api/feedback/get";
+  if (count) {
+    url = `/api/feedback/get?count=${count}`;
+  }
+  await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+    },
+    body: JSON.stringify({
+      session_id: localStorage.getItem("dsgt-portal-session-key"),
+      feedback_type: type,
+    }),
+  }).then(async (res) => {
+    const json = await res.json();
+    if (!json.ok && json.error) {
+      throw new Error(json.error);
+    } else {
+      if (callback) callback(json.data);
+    }
+  });
+};
+
 export const createFeedback = async (
   satisfaction?: string,
   action?: string,
--- client/src/Pages/PortalFeedback/PortalFeedback.tsx
+++ client/src/Pages/PortalFeedback/PortalFeedback.tsx
@@ -1,12 +1,64 @@
-import React, { FC } from 'react';
-import styles from './PortalFeedback.module.scss';
+import React, { FC, useEffect, useState } from "react";
+import {
+  FeedbackType,
+  getFeedback,
+  result_getFeedback,
+} from "../../API/Feedback";
+import ErrorText from "../../components/ErrorText/ErrorText";
+import InputDropdown from "../../components/InputDropdown/InputDropdown";
+import styles from "./PortalFeedback.module.scss";
 
 interface PortalFeedbackProps {}
 
-const PortalFeedback: FC<PortalFeedbackProps> = () => (
-  <div className={styles.PortalFeedback} data-testid="PortalFeedback">
-    PortalFeedback Component
-  </div>
-);
+const PortalFeedback: FC<PortalFeedbackProps> = () => {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType>(
+    FeedbackType.Bugs
+  );
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [feedback, setFeedback] = useState<result_getFeedback[]>([]);
+
+  useEffect(() => {
+    //get feedback
+    getFeedback(feedbackType, undefined, (data) => {
+      setFeedback(data);
+    })
+      .catch((err) => {
+        setError(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [feedbackType]);
+
+  const handleTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    let newValue = e.currentTarget.value;
+    if (!newValue) {
+      return;
+    }
+    setFeedbackType(newValue as FeedbackType);
+  };
+  return (
+    <div className={styles.PortalFeedback} data-testid="PortalFeedback">
+      <h1 className={styles.Major}>Feedback</h1>
+      <h2 className={styles.Minor}>Select Type:</h2>
+      <InputDropdown
+        options={Object.keys(FeedbackType)}
+        initialValue={feedbackType}
+        onChange={handleTypeChange}
+      />
+      <ErrorText>{error}</ErrorText>
+      {loading ? (
+        <p>loading...</p>
+      ) : (
+        <div>
+          {feedback.map((f) => (
+            <p>{f.action}</p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default PortalFeedback;
