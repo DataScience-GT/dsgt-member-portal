d643202143edec265e58ba13f28dbcf239822fd7
JohnRamberger <jramberger3@gatech.edu>
2022-09-02 14:28:15 -0400

    email fix for Sai Avunuri

--- data/seeds/moderators.ts
+++
@@ -1,8 +0,0 @@
-import { Knex } from "knex";
-
-let emails = [];
-
-export async function seed(knex: Knex): Promise<void> {
-  // change user roles to moderator
-  // await knex("user").update({role: "administrator"}).where("email", "ssaraf33@gatech.edu");
-}
