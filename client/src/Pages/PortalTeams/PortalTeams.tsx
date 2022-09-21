ee7855f432b7241dcbc0467cd2094d28e643611d
JohnRamberger <jramberger3@gatech.edu>
2022-09-20 21:56:29 -0400

    Update PortalTeams.tsx
    setup PortalTeams for multiple views

--- client/src/Pages/PortalTeams/PortalTeams.tsx
+++ client/src/Pages/PortalTeams/PortalTeams.tsx
@@ -1,12 +1,35 @@
-import React, { FC } from 'react';
-import styles from './PortalTeams.module.scss';
+import React, { FC } from "react";
+import styles from "./PortalTeams.module.scss";
 import portal_styles from "../PortalPage.module.scss";
+import { Route, Routes } from "react-router-dom";
+import FlexColumn from "../../layout/FlexColumn/FlexColumn";
+import FlexRow from "../../layout/FlexRow/FlexRow";
+import TeamCard from "../../components/TeamCard/TeamCard";
 
 interface PortalTeamsProps {}
 
 const PortalTeams: FC<PortalTeamsProps> = () => (
-  <div className={`${styles.PortalTeams} ${portal_styles.PortalPage}`} data-testid="PortalTeams">
+  <div
+    className={`${styles.PortalTeams} ${portal_styles.PortalPage}`}
+    data-testid="PortalTeams"
+  >
     <h1 className={portal_styles.Major}>Teams</h1>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <FlexColumn>
+            <h2 className={styles.Minor}>All Teams</h2>
+            <FlexRow>
+              <TeamCard />
+              <TeamCard />
+              <TeamCard />
+            </FlexRow>
+          </FlexColumn>
+        }
+      />
+      <Route path="/:id" element={<p>two</p>} />
+    </Routes>
   </div>
 );
 
