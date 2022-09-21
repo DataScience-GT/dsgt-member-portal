06ec18e67bf2f33922434e72d7d9c9867ab4f5c5
JohnRamberger <jramberger3@gatech.edu>
2022-09-20 22:32:04 -0400

    Updated TeamCard
    updated styling, added Name, Id

--- client/src/components/TeamCard/TeamCard.tsx
+++ client/src/components/TeamCard/TeamCard.tsx
@@ -1,11 +1,18 @@
-import React, { FC } from 'react';
-import styles from './TeamCard.module.scss';
+import React, { FC } from "react";
+import FlexColumn from "../../layout/FlexColumn/FlexColumn";
+import styles from "./TeamCard.module.scss";
 
-interface TeamCardProps {}
+interface TeamCardProps {
+  id?: number;
+  name?: string;
+}
 
-const TeamCard: FC<TeamCardProps> = () => (
+const TeamCard: FC<TeamCardProps> = ({ id, name }: TeamCardProps) => (
   <div className={styles.TeamCard} data-testid="TeamCard">
-    TeamCard Component
+    <FlexColumn gap="0.5em">
+      <p className={styles.Name}>{name || "No Name"}</p>
+      {id ? <a href={`/portal/teams/${id}`} className={styles.Button}>Details</a> : ""}
+    </FlexColumn>
   </div>
 );
 
