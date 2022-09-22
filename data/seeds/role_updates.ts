7c477da326d0d63479af3a0e6dc3bb33b7861357
JohnRamberger <jramberger3@gatech.edu>
2022-09-22 13:24:33 -0400

    Update role_updates.ts
    setup for local account creation

--- data/seeds/role_updates.ts
+++ data/seeds/role_updates.ts
@@ -1,11 +1,14 @@
 import { Knex } from "knex";
+import { checkUserEmail } from "../../model";
 
 export async function seed(knex: Knex): Promise<void> {
   // Inserts seed entries
   if (process.env.NODE_ENV == "development") {
     await knex("billing_details").insert({ email: "test@test.com" });
-    await knex("user")
-      .update({ role: "developer" })
-      .where({ email: "test@test.com" });
+    if (await checkUserEmail("test@test.com")) {
+      await knex("user")
+        .update({ role: "developer" })
+        .where({ email: "test@test.com" });
+    }
   }
 }
