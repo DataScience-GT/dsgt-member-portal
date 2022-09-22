ff41f42da2382caba6698b451bef1d78f08a0526
JohnRamberger <jramberger3@gatech.edu>
2022-09-22 14:55:19 -0400

    Updated tests for api
    added some new tests, changed formatting of files. Setup auth

---
+++ tests/api/api.test.ts
@@ -0,0 +1,51 @@
+import app from "../../app";
+import supertest from "supertest";
+
+import bearer from "../bearer";
+
+describe("Test /api", () => {
+  test("GET /api/", (done) => {
+    supertest(app)
+      .get("/api")
+      .expect(200)
+      .then((res) => {
+        expect(res.text).toEqual("welcome to the api!");
+        done();
+      })
+      .catch(done);
+  });
+  test("/api/auth without auth", (done) => {
+    supertest(app)
+      .get("/api/auth")
+      .expect(400)
+      .then((res) => {
+        expect(res.body.ok).toBe(0);
+        expect(res.body.error).toBe("missing authorization header");
+        done();
+      })
+      .catch(done);
+  });
+  test("/api/auth with invalid auth", (done) => {
+    supertest(app)
+      .get("/api/auth")
+      .set("Authorization", "Bearer sdkfl")
+      .expect(401)
+      .then((res) => {
+        expect(res.body.ok).toBe(0);
+        expect(res.body.error).toBe("invalid bearer token");
+        done();
+      })
+      .catch(done);
+  });
+  test("/api/auth with auth", (done) => {
+    supertest(app)
+      .get("/api/auth")
+      .set("Authorization", bearer)
+      .expect(200)
+      .then((res) => {
+        expect(res.body.ok).toBe(1);
+        done();
+      })
+      .catch(done);
+  });
+});
---
+++ tests/user/user.login.test.ts
@@ -0,0 +1,33 @@
+import app from "../../app";
+import supertest from "supertest";
+
+import bearer from "../bearer";
+
+describe("Test /api/user/login", () => {
+  test("/api/user/login with no auth", (done) => {
+    supertest(app)
+      .post("/api/user/login")
+      // .set("Authorization", bearer)
+      // .send({})
+      .expect(400)
+      .then((res) => {
+        expect(res.body.ok).toBe(0);
+        expect(res.body.error).toBe("missing authorization header");
+        done();
+      })
+      .catch(done);
+  });
+  test("/api/user/login with auth no body", (done) => {
+    supertest(app)
+      .post("/api/user/login")
+      .set("Authorization", bearer)
+      // .send({})
+      .expect(400)
+      .then((res) => {
+        expect(res.body.ok).toBe(0);
+        expect(res.body.error).toBe("Missing 1 or more required fields.");
+        done();
+      })
+      .catch(done);
+  });
+});
