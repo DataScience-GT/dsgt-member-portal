3d0d7de54a08eb288afc576531515bbdc664dd14
JohnRamberger <jramberger3@gatech.edu>
2022-09-22 13:18:32 -0400

    Updated Seeds
    removed extraneous, added local test seed for creating account.

--- data/seeds/role_updates.ts
+++ data/seeds/role_updates.ts
@@ -1,6 +1,11 @@
 import { Knex } from "knex";
 
 export async function seed(knex: Knex): Promise<void> {
-    // Inserts seed entries
-    await knex("user").update({role: "administrator"}).where("email", "ssaraf33@gatech.edu");
-};
+  // Inserts seed entries
+  if (process.env.NODE_ENV == "development") {
+    await knex("billing_details").insert({ email: "test@test.com" });
+    await knex("user")
+      .update({ role: "developer" })
+      .where({ email: "test@test.com" });
+  }
+}
--- data/seeds/roles_update.ts
+++
@@ -1,7 +0,0 @@
-import { Knex } from "knex";
-
-export async function seed(knex: Knex): Promise<void> {
-  await knex("user")
-    .update({ role: "developer" })
-    .where("email", "rambergerjohn@gmail.com");
-}
