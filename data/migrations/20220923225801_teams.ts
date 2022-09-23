15688f57b4b0c9f02044813dd54ba5aecd227695
JohnRamberger <jramberger3@gatech.edu>
2022-09-23 19:58:01 -0400

    Update 20220923225801_teams.ts
    teams knex migration

--- data/migrations/20220923225801_teams.ts
+++ data/migrations/20220923225801_teams.ts
@@ -2,8 +2,8 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("teams", (table) => {
-    table.increments("team_id");
-    
+    table.increments("team_id").primary();
+    table.string("name");
   });
 }
 
