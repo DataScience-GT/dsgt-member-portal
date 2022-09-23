52c7bfb1bbc2b0b793967f5be80c55546a8bf7be
JohnRamberger <jramberger3@gatech.edu>
2022-09-22 22:38:08 -0400

    Updated PortalTeams
    added features to TeamPage such as inputting a list of emails which are then sent to a ScrollableList.

--- client/src/Pages/PortalTeams/PortalTeams.tsx
+++ client/src/Pages/PortalTeams/PortalTeams.tsx
@@ -10,6 +10,7 @@ import Form from "../../components/Form/Form";
 import InputField from "../../components/InputField/InputField";
 import { handleChange_input_string } from "../../Scripts/InputHandler";
 import { compareUserRoles } from "../../Scripts/RoleManagement";
+import ScrollableList from "../../components/ScrollableList/ScrollableList";
 
 interface PortalTeamsProps {
   role?: string;
@@ -89,7 +90,7 @@ const PortalTeams: FC<PortalTeamsProps> = ({ role }) => {
             </>
           }
         />
-        <Route path="/:team_id" element={<TeamPage />} />
+        <Route path="/:team_id" element={<TeamPage role={role} />} />
       </Routes>
     </div>
   );
@@ -101,22 +102,62 @@ interface TeamPageProps {
 }
 const TeamPage: FC<TeamPageProps> = ({ role }) => {
   const { team_id } = useParams();
+  const [emails, setEmails] = useState<Set<string>>();
+
+  const handleChange_emails = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    let newValue = e.currentTarget.value;
+    let s: Set<string> = new Set();
+    let new_line = newValue.split("\n");
+    let semicolon = newValue.split(";");
+    let comma = newValue.split(",");
+    let vals;
+    if (new_line.length > semicolon.length && new_line.length > comma.length) {
+      vals = new_line;
+    } else if (
+      semicolon.length > new_line.length &&
+      semicolon.length > comma.length
+    ) {
+      vals = semicolon;
+    } else {
+      vals = comma;
+    }
+
+    vals.forEach((v) => {
+      if (v && v.includes("@") && v.includes(".")) s.add(v.trim());
+    });
+    setEmails(s);
+  };
+
   return (
     <div className={styles.TeamPage}>
+      <InlineLink to="/portal/teams" margin="1em 0 0 0">
+        Go Back
+      </InlineLink>
       {compareUserRoles(role || "guest", "administrator") >= 0 ? (
-        <>
-          <InlineLink to="/portal/teams" margin="1em 0 0 0">
-            Go Back
-          </InlineLink>
-        </>
+        <FlexColumn gap="1em" padding="1em 0 0 0">
+          <h2 className={portal_styles.Minor}>Add users to team</h2>
+
+          <FlexRow gap="1em">
+            <textarea
+              className={styles.TextInput}
+              onChange={handleChange_emails}
+            ></textarea>
+
+            <>
+              <ScrollableList
+                height="15em"
+                minWidth="15em"
+                width="fit-content"
+                values={emails ? Array.from(emails) : []}
+              />
+            </>
+          </FlexRow>
+        </FlexColumn>
       ) : (
         <>
           <h2 className={portal_styles.Minor}>
             You do not have access to this page.
           </h2>
-          <InlineLink to="/portal/teams" margin="1em 0 0 0">
-            Go Back
-          </InlineLink>
         </>
       )}
     </div>
