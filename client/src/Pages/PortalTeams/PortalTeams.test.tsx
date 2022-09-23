4f6d304856b5bdf38ce800f75a8d02c8c98372de
JohnRamberger <jramberger3@gatech.edu>
2022-09-22 22:38:15 -0400

    Update PortalTeams.test.tsx
    fixed initial test case

--- client/src/Pages/PortalTeams/PortalTeams.test.tsx
+++ client/src/Pages/PortalTeams/PortalTeams.test.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import PortalTeams from './PortalTeams';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import PortalTeams from "./PortalTeams";
+import { BrowserRouter } from "react-router-dom";
 
-describe('<PortalTeams />', () => {
-  test('it should mount', () => {
-    render(<PortalTeams />);
-    
-    const portalTeams = screen.getByTestId('PortalTeams');
+describe("<PortalTeams />", () => {
+  test("it should mount", () => {
+    render(<PortalTeams />, { wrapper: BrowserRouter });
+
+    const portalTeams = screen.getByTestId("PortalTeams");
 
     expect(portalTeams).toBeInTheDocument();
   });
