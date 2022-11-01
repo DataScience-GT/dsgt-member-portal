6e9b574841510c41755ecc7133d4a4a0dd72e5be
JohnRamberger <jramberger3@gatech.edu>
2022-10-31 20:58:43 -0400

    Updated PortalChecking
    added request to get all users checked in for all events

--- client/src/API/Checkin.ts
+++ client/src/API/Checkin.ts
@@ -78,3 +78,34 @@ export const checkinUser = async (
     }
   });
 };
+
+export type result_getCheckinUsers = {
+  checkin_id: number;
+  user_id: number;
+  event_id: number;
+  created_at: string;
+  created_by: number;
+};
+
+export const getCheckinUsers = async (
+  event_id?: number,
+  callback?: (data: result_getCheckinUsers[]) => void
+) => {
+  let url = `/api/checkin/users/get?session_id=${localStorage.getItem(
+    "dsgt-portal-session-key"
+  )}${event_id ? `&event_id=${event_id}` : ""}`;
+  await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+    },
+  }).then(async (res) => {
+    const json = await res.json();
+    if (!json.ok && json.error) {
+      throw new Error(json.error);
+    } else {
+      if (callback) callback(json.data as result_getCheckinUsers[]);
+    }
+  });
+};
