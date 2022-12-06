a1d3b54e2ff2af50882965fd484d5c19ea291517
JohnRamberger <jramberger3@gatech.edu>
2022-12-05 23:25:29 -0500

    Update AnnouncementEmail.ts

--- EmailTemplates/AnnouncementEmail.ts
+++ EmailTemplates/AnnouncementEmail.ts
@@ -4,10 +4,4 @@ export const getAnnouncementEmailTemplate = (emailBody: string) => {
     <div style="background: #aaa; padding: 10px;">${emailBody}</div>
     <p>You are receiving this email because you selected OK after asked for DSGT email communication consent. If you would like to stop receiving these emails, reach out to John Ramberger at john@datasciencegt.org.</p>
     `;
-
-  // return "<p>New announcement from the Data Science club at Georgia Tech!</p>"
-  //         + emailBody
-  //         + "<p>You are receiving this email because you selected OK after asked</p>"
-  //         + "<p>for DSGT email communication consent. If you would like to stop receiving</p>"
-  //         + "<p>these emails, reach out to John Ramberger at john@datasciencegt.org.</p>";
 };
