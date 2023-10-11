84e4930611343896fe39288dc832245a8e2ff68a
Vicente Miranda <vmiranda6@gatech.edu>
2023-10-11 16:48:13 -0400

    tweak

--- app.ts
+++ app.ts
@@ -11,7 +11,8 @@ app.use(express.json({ limit: "50mb" }));
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
-  "http://localhost:3002"
+  "http://localhost:3002",
+  "https://datasciencegt.org"
 ]
 
 const corsOptions = {
