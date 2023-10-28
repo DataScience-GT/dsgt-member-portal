00d7f69a5c8cdcd29addd15a15501bd96b7fa0bf
Vicente Miranda <vmiranda6@gatech.edu>
2023-10-28 17:29:33 -0400

    sidebar updates

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -69,6 +69,9 @@ const Sidebar: FC<SidebarProps> = ({ role }: SidebarProps) => {
     }
   };
 
+  /**
+   * This toggles the side bar to be open or closed
+   */
   const toggleOpen = () => {
     if (open) {
       setOpen(false);
