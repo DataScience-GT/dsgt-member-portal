ca37c43413a63625f363238dded272399b82ff39
stadimeti19 <stadimeti3@gatech.edu>
2023-11-30 20:29:31 -0500

    Updated the project submission form to fix the margin spacing for some of the Question text and the InputField so that they are not squished together

--- client/src/components/ProjectCard/ProjectCard.tsx
+++ client/src/components/ProjectCard/ProjectCard.tsx
@@ -44,77 +44,77 @@ const ProjectCard: FC<ProjectCardProps> = ({
     onApply,
 }: ProjectCardProps) => {
 
-    return (
-        <div className={styles.ProjectCard} data-testid="ProjectCard">
-            {deletable ? (
-                <div className={styles.TrashContainer}>
-                    <img className={styles.Trash} src={trash_icon} alt="click to delete" />
-                </div>
-            ) : (
-                ""
-            )}
-            <div className={styles.ProjectCardInfo}>
-                <FlexColumn padding="1em" gap="0.5em">
-                    <h3 className={styles.Major}>{pname}</h3>
-                    <p className={styles.ProjectInfo}> 
-                        <span className={styles.Span}>Start date</span> 
-                        {startDate}
-                    </p>
-                    <p className={styles.ProjectInfo}> 
-                        <span className={styles.Span}>Description</span> 
-                        {pdescription}
-                    </p>
-                    <p className={styles.ProjectInfo}> 
-                        <span className={styles.Span}>Length of project</span> 
-                        {termLength}
-                    </p>
-                    <p className={styles.ProjectInfo}> 
-                        <span className={styles.Span}>Hourly compensation</span> 
-                        {compensationHour}
-                    </p>
-                    <p className={styles.ProjectInfo}> 
-                        <span className={styles.Span}>Location</span> 
-                        {plocation}
-                    </p>
-                    <p className={styles.ProjectInfo}> 
-                        <span className={styles.Span}>Number of openings</span> 
-                        {numStudents}
-                    </p>
-                    <p className={styles.ProjectInfo}> 
-                        <span className={styles.Span}>Related fields</span> 
-                        {relatedFields}
-                    </p>
-                    <p className={styles.ProjectInfo}> 
-                        <span className={styles.Span}>Desired skills</span> 
-                        {desiredSkills}
-                    </p>
-                    <p className={styles.ProjectInfo}> 
-                        <span className={styles.Span}>Project hosts</span> 
-                        {phosts}
-                    </p>
-                    <p className={styles.ProjectInfo}> 
-                        <span className={styles.Span}>Contact email</span> 
-                        {contactEmail}
-                    </p>
-                </FlexColumn>
-            </div>
-            {applyable ? (
-                <div className={styles.ProjectCardApp}>
-                    <div className={styles.Submit}>
-                        <InputField type="submit" placeholder="Apply" width="auto" 
-                            onClick={() => {
-                                if (pid && onApply) {
-                                    onApply(pid);
-                                }
-                            }} 
-                        />
-                    </div>
-                </div>
-            ) : (
-                ""
-            )}
-        </div>
-    )
-}
+     return (
+         <div className={styles.ProjectCard} data-testid="ProjectCard">
+             {deletable ? (
+                 <div className={styles.TrashContainer}>
+                     <img className={styles.Trash} src={trash_icon} alt="click to delete" />
+                 </div>
+             ) : (
+                 ""
+             )}
+             <div className={styles.ProjectCardInfo}>
+                 <FlexColumn padding="1em" gap="0.5em">
+                     <h3 className={styles.Major}>{pname}</h3>
+                     <p className={styles.ProjectInfo}> 
+                         <span className={styles.Span}>Start date</span> 
+                         {startDate}
+                     </p>
+                     <p className={styles.ProjectInfo}> 
+                         <span className={styles.Span}>Description</span> 
+                         {pdescription}
+                     </p>
+                     <p className={styles.ProjectInfo}> 
+                         <span className={styles.Span}>Length of project</span> 
+                         {termLength}
+                     </p>
+                     <p className={styles.ProjectInfo}> 
+                         <span className={styles.Span}>Hourly compensation</span> 
+                         {compensationHour}
+                     </p>
+                     <p className={styles.ProjectInfo}> 
+                         <span className={styles.Span}>Location</span> 
+                         {plocation}
+                     </p>
+                     <p className={styles.ProjectInfo}> 
+                         <span className={styles.Span}>Number of openings</span> 
+                         {numStudents}
+                     </p>
+                     <p className={styles.ProjectInfo}> 
+                         <span className={styles.Span}>Related fields</span> 
+                         {relatedFields}
+                     </p>
+                     <p className={styles.ProjectInfo}> 
+                         <span className={styles.Span}>Desired skills</span> 
+                         {desiredSkills}
+                     </p>
+                     <p className={styles.ProjectInfo}> 
+                         <span className={styles.Span}>Project hosts</span> 
+                         {phosts}
+                     </p>
+                     <p className={styles.ProjectInfo}> 
+                         <span className={styles.Span}>Contact email</span> 
+                         {contactEmail}
+                     </p>
+                 </FlexColumn>
+             </div>
+             {applyable ? (
+                 <div className={styles.ProjectCardApp}>
+                     <div className={styles.Submit}>
+                         <InputField type="submit" placeholder="Apply" width="auto" 
+                             onClick={() => {
+                                 if (pid && onApply) {
+                                     onApply(pid);
+                                 }
+                             }} 
+                         />
+                     </div>
+                 </div>
+             ) : (
+                 ""
+             )}
+         </div>
+     )
+ }
 
 export default ProjectCard;
